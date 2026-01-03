76382276cd60d0f8e63b9ab9d14b89624bb6ad74
HliebKurtushyn <kurtgleb007@gmail.com>
2026-01-03 20:53:32 +0100

    fix(User): add default values for email, age and phone

--- account/models.py
+++ account/models.py
@@ -3,9 +3,9 @@ from django.db import models
 
 class User(AbstractUser):
     username = None
-    email = models.EmailField(unique=True)
-    age = models.PositiveIntegerField(null=True, blank=True)
-    phone = models.CharField(max_length=15, unique=True)
+    email = models.EmailField(unique=True, default="")
+    age = models.PositiveIntegerField(null=True, blank=True, default=None)
+    phone = models.CharField(max_length=15, unique=True, default="")
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
