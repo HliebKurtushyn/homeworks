bf211f103818900125edc77408a84c9b06c4ec34
HliebKurtushyn <kurtgleb007@gmail.com>
2026-01-03 20:29:26 +0100

    fix: add missing fields (age and phone)

--- account/models.py
+++ account/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True)
+    age = models.PositiveIntegerField(null=True, blank=True)
+    phone = models.CharField(max_length=15, unique=True)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
