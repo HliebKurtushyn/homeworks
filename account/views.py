113c9da575fa9a1769a2e1d97f89ac669e2c02f0
HliebKurtushyn <kurtgleb007@gmail.com>
2026-01-04 13:06:59 +0100

    feat: add login_view function

--- account/views.py
+++ account/views.py
@@ -1,15 +1,34 @@
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate
 from django.shortcuts import render, redirect
-from .forms import StudentRegistrationForm
+from .forms import StudentRegistrationForm, StudentAuthForm
 
-def register(request):
+from django.http import JsonResponse
+
+def register_view(request):
+    if request.user.is_authenticated:
+        return redirect("dashboard_home")
+
     if request.method == "POST":
         form = StudentRegistrationForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect("dashboard_home")
+            # user = form.save()
+            # login(request, user)
+            # return redirect("dashboard_home")
+            return JsonResponse({"register_status": "success"})
     else:
         form = StudentRegistrationForm()
     
     return render(request, "account/register.html", {"form": form})
+
+
+def login_view(request):    
+    if request.method == "POST":
+        form = StudentAuthForm(request, data=request.POST)
+        if form.is_valid():
+            # login(request, form.get_user())
+            # return redirect("dashboard_home")
+            return JsonResponse({"login_status": "success"})
+    else:
+        form = StudentAuthForm()
+    
+    return render(request, "account/login.html", {"form": form})
