24fd964f1d27605bef80b8c6e0928df7423d7054
HliebKurtushyn <kurtgleb007@gmail.com>
2026-01-05 13:34:47 +0100

    feat: make Course model

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from account.models import User
 
-# Create your models here.
+
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    image = models.CharField(max_length=100, default="dashboard/default.png")
+
+    def __str__(self):
+        return self.name
+    
+User.add_to_class('courses', models.ManyToManyField(Course, blank=True))
