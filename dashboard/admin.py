b3d1eacd3516b950dffdca6710fa0be3a95170da
HliebKurtushyn <kurtgleb007@gmail.com>
2026-01-05 13:34:36 +0100

    fix: make custon UserAdmin

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from account.models import User
+from .models import Course
 
-# Register your models here.
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+# Також цей момент я зробив з ШІ, тому що заплутався що треба прибрати, поставити і так далі
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    filter_horizontal = ('courses', 'groups', 'user_permissions')
+    ordering = ('email',)
+    list_display = ('email', 'is_staff', 'is_active')
+    search_fields = ('email',)
+    
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                    'groups', 'user_permissions')}),
+        ('Courses', {'fields': ('courses',)}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active', 'courses')}
+        ),
+    )
