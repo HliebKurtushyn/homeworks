35936a395319ccf7e9cbb862490e98ffde0f6095
HliebKurtushyn <kurtgleb007@gmail.com>
2026-01-05 13:35:19 +0100

    feat: add dashboard/home view

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Course
+
+@login_required
+def home_view(request):
+    user_courses = request.user.courses.all()
+    user = request.user
+
+    return render(request, "dashboard/home.html", {"courses": user_courses, "user": user})
